f5bd82a184b0cf3bd0146cf62ced98faa81f6cca
Johan Solbakken <johsol@stud.ntnu.no>
2022-11-09 07:39:52 +0100

    Johan og Nora sin kode

--- leesah-game-client/main.py
+++ leesah-game-client/main.py
@@ -1,5 +1,8 @@
 from client_lib import quiz_rapid
 from client_lib.config import HOSTED_KAFKA
+import base64
+import requests
+import json
 
 # LEESAH QUIZ GAME CLIENT
 
@@ -8,8 +11,8 @@ from client_lib.config import HOSTED_KAFKA
 
 # Config ##########################################################################################################
 
-TEAM_NAME = "CHANGE ME"
-QUIZ_TOPIC = "CHANGE ME"
+TEAM_NAME = "TeamImsdal"
+QUIZ_TOPIC = "quiz-rapid"
 CONSUMER_GROUP_ID = f"cg-leesah-team-${TEAM_NAME}-1"
 assert TEAM_NAME is not None and TEAM_NAME != "CHANGE ME", "Husk å gi teamet ditt et navn"
 assert QUIZ_TOPIC is not None and QUIZ_TOPIC != "CHANGE ME", "Husk å sett riktig topic navn"
@@ -20,25 +23,173 @@ assert QUIZ_TOPIC is not None and QUIZ_TOPIC != "CHANGE ME", "Husk å sett rikti
 class MyParticipant(quiz_rapid.QuizParticipant):
     def __init__(self):
         super().__init__(TEAM_NAME)
+        self.dedup = []
+        with open("dedup.txt", "r") as f:
+            for line in f:
+                self.dedup.append(line)
+        with open("saldo.txt", "w") as f:
+            f.write("0")
 
     def handle_question(self, question: quiz_rapid.Question):
-        raise NotImplementedError("Her må du implementere håndtering av spørsmål 😎")
-        # if question.category == "team-registration":
-        #     self.handle_register_team(question)
+        if question.category == "team-registration":
+            self.handle_register_team(question)
+        if question.category == "arithmetic":
+            self.handle_arithmetic(question)
+        if question.category == "ping-pong":
+            self.handle_ping(question)
+        if question.category == "NAV":
+            self.handle_nav(question)
+        if question.category == "base64":
+            self.handle_base64(question)
+        if question.category == "is-a-prime":
+            self.handle_prime(question)
+        if question.category == "deduplication":
+            self.handle_deduplication(question)
+        if question.category == "transactions":
+            self.handle_transaction(question)
+        if question.category == "grunnbelop":
+            self.handle_grunn(question)
 
     def handle_assessment(self, assessment: quiz_rapid.Assessment):
-        # Her kan du implementere feks loggig av assessments om du ønsker
         pass
 
 # Question handlers ################################################################################################
 
-    # def handle_register_team(self, question: quiz_rapid.Question):
-    #     self.publish_answer(
-    #         question_id=question.messageId,
-    #         category=question.category,
-    #         answer=TEAM_NAME
-    #     )
+    def handle_register_team(self, question: quiz_rapid.Question):
+        if question.question == "Register a new team":
+            self.publish_answer(
+                question_id=question.messageId,
+                category=question.category,
+                answer=TEAM_NAME
+            )
 
+        
+
+    def handle_arithmetic(self, question: quiz_rapid.Question):
+        a, operator, b = question.question.split(" ")
+        a=int(a)
+        b=int(b)
+        if operator == "+":
+            self.publish_answer(
+                question_id=question.messageId,
+                category=question.category,
+                answer=a + b
+            )
+        if operator == "-":
+            self.publish_answer(
+                question_id=question.messageId,
+                category=question.category,
+                answer=a - b
+            )
+        if operator == "*":
+            self.publish_answer(
+                question_id=question.messageId,
+                category=question.category,
+                answer=a * b
+            )
+        if operator == "/":
+            self.publish_answer(
+                question_id=question.messageId,
+                category=question.category,
+                answer=int(a / b)
+            )
+    
+    def handle_ping(self, question: quiz_rapid.Question):
+        if question.question == "ping":
+            self.publish_answer(
+                question_id=question.messageId,
+                category=question.category,
+                answer="pong"
+            )
+        if question.question == "pong":
+            self.publish_answer(
+                question_id=question.messageId,
+                category=question.category,
+                answer="ping"
+            )
+
+    def handle_nav(self, question: quiz_rapid.Question):
+        if question.question == "P\u00e5 hvilken nettside finner man informasjon om rekruttering til NAV IT?":
+            self.publish_answer(
+                question_id=question.messageId,
+                category=question.category,
+                answer="detsombetyrnoe.no"
+            )
+        if question.question == "Hva heter applikasjonsplattformen til NAV?":
+            self.publish_answer(
+                question_id=question.messageId,
+                category=question.category,
+                answer="NAIS"
+            )
+
+    def handle_base64(self, question: quiz_rapid.Question):
+        value = question.question[5:]
+        self.publish_answer(
+                question_id=question.messageId,
+                category=question.category,
+                answer=base64.b64decode(value).decode('ascii')
+            )
+    
+    def handle_prime(self, question: quiz_rapid.Question):
+        ans = True
+        n = int(question.question.split(" ")[-1])
+
+        for i in range(2, n):
+            if (n % i) == 0:
+                ans = False
+                break
+        self.publish_answer(
+            question_id=question.messageId,
+            category=question.category,
+            answer=ans
+        )
+
+    def handle_deduplication(self, question: quiz_rapid.Question):
+        if question.question not in self.dedup:
+            self.dedup.append(question.question)
+            with open("dedup.txt", "a") as f:
+                f.write(question.question + "\n")
+            self.publish_answer(
+            question_id=question.messageId,
+            category=question.category,
+            answer="you wont dupe me!"
+            )
+        """self.publish_answer(
+            question_id=question.messageId,
+            category=question.category,
+            answer="you duped me!"
+            )"""
+
+    def handle_transaction(self, question: quiz_rapid.Question):
+        action, value = question.question.split(" ")
+        value = int(value)
+        saldo = 0
+        with open("saldo.txt", "r") as f:
+            saldo = int(f.read())
+        if (action == "UTTREKK"):
+            saldo -= value
+        else:
+            saldo += value
+        with open("saldo.txt", "w") as f:
+            f.write(str(saldo))
+        self.publish_answer(
+            question_id=question.messageId,
+            category=question.category,
+            answer=saldo
+            )
+
+    def handle_grunn(self, question: quiz_rapid.Question):
+        dato = question.question.split(" ")[-1]
+        req = requests.get("http://g.nav.no/api/v1/grunnbeløp?name={}".format(dato))
+        data = req.json()
+        self.publish_answer(
+            question_id=question.messageId,
+            category=question.category,
+            answer=str(data["grunnbeløp"])
+            )
+         
+
+        
 #####################################################################################################################
 
 
@@ -52,6 +203,6 @@ def main():
         logg_questions=True,   # Logg spørsmålene appen mottar
         logg_answers=True,     # Logg svarene appen sender
         short_log_line=False,  # Logg bare en forkortet versjon av meldingene
-        log_ignore_list=[]     # Liste med spørsmålskategorier loggingen skal ignorere
+        log_ignore_list=["arithmetic", "team-registration", "ping-pong", "base64", "is-a-prime", "deduplication"]     # Liste med spørsmålskategorier loggingen skal ignorere
     )
     return MyParticipant(), rapid
